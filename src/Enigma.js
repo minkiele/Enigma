f923d09658f8f93511eaa1cdf24127b895bca734
Michele Montagner <michelasso@gmail.com>
2016-09-18 23:28:46 +0200

    Added some monae

--- src/Enigma.js
+++ src/Enigma.js
@@ -1,7 +1,10 @@
+import Plugboard from "./PlugBoard";
+import {A_INDEX, getNextLetter} from "./Utils";
+
 export default class Enigma {
 
   constructor () {
-
+    this.plugboard = new PlugBoard();
   }
 
   setLeftRotor (leftRotor) {
@@ -12,6 +15,10 @@ export default class Enigma {
     this.leftRotorLetter = leftRotorLetter.toUpperCase();
   }
 
+  advanceLeftRotor () {
+    this.leftRotorLetter = getNextLetter(this.leftRotorLetter);
+  }
+
   setCenterRotor (centerRotor) {
     this.centerRotor = centerRotor;
   }
@@ -20,12 +27,24 @@ export default class Enigma {
     this.centerRotorLetter = centerRotorLetter.toUpperCase();
   }
 
+  advanceCenterRotor () {
+    this.centerRotorLetter = getNextLetter(this.centerRotorLetter);
+  }
+
   setRightRotor (rightRotor) {
     this.rightRotor = rightRotor;
   }
 
   setRightRotorLetter (rightRotorLetter) {
     this.rightRotorLetter = rightRotorLetter.toUpperCase();
+  }
+
+  advanceRightRotor () {
+    this.rightRotorLetter = getNextLetter(this.rightRotorLetter);
+  }
+
+  encodeLetter (letter) {
+
   }
 
 }
--- src/Utils.js
+++ src/Utils.js
@@ -1,3 +1,9 @@
 const A_INDEX = 'A'.charCodeAt(0);
 
-export {A_INDEX};
+function getNextLetter (currentLetter) {
+    var currentIndex = currentLetter.charCodeAt(0) - A_INDEX;
+    var nextIndex = (currentIndex + 1) % 26;
+    return String.fromCharCode(nextIndex + A_INDEX);
+}
+
+export {A_INDEX, getNextLetter};
