e552ec5518995e06575f95b9756f667c68eb8031
Michele Montagner <michele.montagner@nttdata.com>
2023-11-02 18:28:03 +0100

    aggiunto il bundle

---
+++ rollup.config.mjs
@@ -0,0 +1,32 @@
+import { babel } from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+
+const extensions = ['.mjs', '.js', '.json', '.node', '.ts'];
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'enigma/bundle.cjs',
+      format: 'cjs',
+    },
+    {
+      file: 'enigma/bundle.mjs',
+      format: 'esm',
+    },
+  ],
+  external: ['events'],
+  plugins: [
+    resolve({
+      extensions,
+    }),
+    commonjs(),
+    babel({
+      extensions,
+      babelHelpers: 'bundled',
+    }),
+    terser(),
+  ],
+};
